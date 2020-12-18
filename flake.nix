ea4945463adcb6b760f8fedf06e3588ac43bd1e3
kranex <oliverstrik@gmail.com>
2020-12-18 15:20:47 +0000

    fixing for work

--- flake.nix
+++ flake.nix
@@ -2,61 +2,27 @@
   description = "Nix is love. Nix is life.";
 
   inputs = {
-
-    stable = {
-      url = "github:NixOS/nixpkgs/release-20.09";
-    };
-
-    unstable = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
-
-    custom = {
-      url = "/etc/nixos/nixpkgs-custom";
-      flake = false;
-    };
-
     secrets-dir = {
-      url = "/etc/nixos/secrets";
+      url = "/mnt/etc/nixos/secrets";
       flake = false;
     };
   };
 
-  outputs = {self, stable, unstable, custom, secrets-dir}:
+  outputs = {self, nixpkgs, secrets-dir}:
     let
-      pkgs = import stable {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-
-      pkgs-unstable = import unstable {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-
-      pkgs-custom = import custom {
-        pkgs = pkgs;
-      };
-
       secrets = import secrets-dir;
     in
     {
-      nixosConfigurations.nixbidium =
-        let
-
-          system = "x86_64-linux";
-
-          specialArgs = {
-            inherit pkgs pkgs-unstable pkgs-custom secrets;
-          };
-
-          modules = [
-            ./hosts/nixbidium/configuration.nix
-            pkgs-custom.modules
-          ];
-
-        in
-
-        stable.lib.nixosSystem { inherit system modules specialArgs; };
+      nixosConfigurations = {
+      	nixogen = nixpkgs.lib.nixosSystem {
+		  system = "x86_64-linux";
+		  specialArgs = {
+		    inherit secrets;
+		  };
+		  modules = [
+		    ./hosts/nixogen/configuration.nix
+		  ];
+	};
+      };
     };
 }
