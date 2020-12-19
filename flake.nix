37d81ef06b177551b9557181fced6c1a7d6d4c11
Kranex <oliverstrik@gmail.com>
2020-12-19 15:45:44 +0100

    nicer unstable override

--- flake.nix
+++ flake.nix
@@ -2,36 +2,48 @@
   description = "Nix is love. Nix is life.";
 
   inputs = {
-    nixosPkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+    # Pin nixpkgs to 20.09
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
 
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Unstable branch
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # My custom packages WIP
     custom = {
       url = "/etc/nixos/nixpkgs-custom";
       flake = false;
     };
 
+    # where the secrets dir is.
     secrets-dir = {
       url = "/etc/nixos/secrets";
       flake = false;
     };
   };
 
-  outputs = inputs@{self, nixosPkgs, ...}: {
-    nixosConfigurations = let
+ outputs = inputs@{self, nixpkgs, ...}:
+    let
+      # overlay unstable on nixpkgs. pkgs.unstable.[package] should now be
+      # available.
+      overlay-unstable = final: prev: {
+        unstable = inputs.nixpkgs-unstable.legacyPackages.${final.system};
+      };
+      # import the secrets dir.
       secrets = import inputs.secrets-dir;
-      unstable = import inputs.unstable { system = "x86_64-linux"; };
-      custom = import inputs.custom { pkgs = nixosPkgs; };
+      # import custom using stable as the base.
+      custom = import inputs.custom { pkgs = nixpkgs; };
     in {
-      nixogen = nixosPkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit secrets unstable custom; };
-        modules = [
-          ./hosts/nixogen/configuration.nix
-          custom.modules
-        ];
-
+      nixosConfigurations = {
+        ## Work Lenovo E15
+        nixogen = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit secrets; };
+          modules = [
+            ({pkgs, ...}: { nixpkgs.overlays = [overlay-unstable]; })
+            ./hosts/nixogen/configuration.nix
+            custom.modules
+          ];
+        };
       };
     };
-  };
 }
