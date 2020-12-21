8c319c0c9fb3d6abf426c4e96cdd9193d78eb1bd
Kranex <oliverstrik@gmail.com>
2020-12-21 23:55:06 +0100

    extracted picom

--- shared/wm/i3.nix
+++ shared/wm/i3.nix
@@ -1,29 +1,67 @@
-{config, pkgs, ...}:
+{config, lib, pkgs, ...}:
 
-{
-  services.xserver = {
-    enable = true;
+with lib;
 
-    desktopManager = {
-      xterm.enable = false;
+let
+  cfge = config.environment;
+  cfg = config.services.xserver.windowManager.i3;
+in
+  {
+    imports = [
+      ./picom.nix
+    ];
+
+    options = {
+      services.xserver.windowManager.i3 = {
+        gaps = {
+          inner = mkOption {
+            type = types.int;
+            default = 0;
+            description = ''
+              The gap in pixels between windows.
+            '';
+          };
+          outer = mkOption {
+            type = types.int;
+            default = 0;
+            description = ''
+              The gap in pixels between the edges.
+            '';
+          };
+        };
+      };
     };
 
-    displayManager = {
-      defaultSession = "none+i3";
+    config.services.xserver = {
+        enable = true;
+
+        desktopManager = {
+          xterm.enable = false;
+        };
+
+        displayManager = {
+          defaultSession = "none+i3";
+        };
+
+        windowManager.i3 = {
+          enable = true;
+          package = pkgs.i3-gaps;
+
+          extraPackages = with pkgs; [
+            rofi
+            i3lock
+            i3status
+          ];
+
+          configFile = (pkgs.writeText "i3-config" (''
+              # Generated Config
+              gaps inner ${builtins.toString cfg.gaps.inner}
+              gaps outer ${builtins.toString cfg.gaps.outer}
+
+              # User Config
+            ''
+            + (builtins.readFile ../../dots/i3.config)));
+        };
+      };
     };
-
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-
-      extraPackages = with pkgs; [
-        rofi
-        i3lock
-        i3status
-      ];
-
-      configFile = ../../dots/i3.config;
-    };
-
-  };
-}
+  }
