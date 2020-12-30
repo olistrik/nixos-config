9d797d9985de1d12a51026165b5985e8e5095114
Kranex <oliverstrik@gmail.com>
2020-12-30 15:53:59 +0000

    home works

---
+++ secrets/default.nix
@@ -0,0 +1,8 @@
+{
+  root = {
+    hashedPassword = "$6$2Xonn.DZ$xCkO89Lc5tFb3e7ZDT3rVG16jfNSRgZ8Nh45.mKghB9UI2HolvpaFjJyRxZZz3EtpaufwFgSUWo6yq3V2NFND1";
+  };
+  kranex = {
+    hashedPassword = "$6$2Xonn.DZ$xCkO89Lc5tFb3e7ZDT3rVG16jfNSRgZ8Nh45.mKghB9UI2HolvpaFjJyRxZZz3EtpaufwFgSUWo6yq3V2NFND1";
+  };
+}
---
+++ secrets/root.nix
@@ -0,0 +1,3 @@
+{
+  hashedPassword = "$6$2Xonn.DZ$xCkO89Lc5tFb3e7ZDT3rVG16jfNSRgZ8Nh45.mKghB9UI2HolvpaFjJyRxZZz3EtpaufwFgSUWo6yq3V2NFND1";
+}
