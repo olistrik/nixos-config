be3d2c09f70de53fa32b18ee0fa487eb0ed9a162
Kranex <oliverstrik@gmail.com>
2021-01-07 11:32:34 +0100

    added ruby/rails to vim

--- shared/programs/zsh.nix
+++ shared/programs/zsh.nix
@@ -33,6 +33,26 @@
     zplug load
 
     #################################
+    ## fix nix
+
+    function _nix() {
+      local ifs_bk="$IFS"
+      local input=("''${(Q)words[@]}")
+      IFS=$'\n'
+      local res=($(NIX_GET_COMPLETIONS=$((CURRENT - 1)) "$input[@]"))
+      IFS="$ifs_bk"
+      local tpe="$res[1]"
+      local suggestions=(''${res:1})
+      if [[ "$tpe" == filenames ]]; then
+        compadd -fa suggestions
+      else
+        compadd -a suggestions
+      fi
+    }
+
+    compdef _nix nix
+
+    #################################
     ## Magic Shit
 
       setopt correct                   # Auto correct mistakes
