ab01d20f7b06964e63c4e85bc2f3907153569dbd
Oliver Strik <oliverstrik@gmail.com>
2021-03-23 14:55:41 +0100

    damn it

--- secrets/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  root = {
-    hashedPassword = "$6$2Xonn.DZ$xCkO89Lc5tFb3e7ZDT3rVG16jfNSRgZ8Nh45.mKghB9UI2HolvpaFjJyRxZZz3EtpaufwFgSUWo6yq3V2NFND1";
-  };
-  kranex = {
-    hashedPassword = "$6$2Xonn.DZ$xCkO89Lc5tFb3e7ZDT3rVG16jfNSRgZ8Nh45.mKghB9UI2HolvpaFjJyRxZZz3EtpaufwFgSUWo6yq3V2NFND1";
-  };
-}
--- secrets/root.nix
+++
@@ -1,3 +0,0 @@
-{
-  hashedPassword = "$6$2Xonn.DZ$xCkO89Lc5tFb3e7ZDT3rVG16jfNSRgZ8Nh45.mKghB9UI2HolvpaFjJyRxZZz3EtpaufwFgSUWo6yq3V2NFND1";
-}
