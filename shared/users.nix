59b1032bcebebdff7094f5a4b3b4f89f0aad0f65
Kranex <oliverstrik@gmail.com>
2021-06-20 16:58:19 +0200

    cleaned up a little.

--- shared/users.nix
+++ shared/users.nix
@@ -1,12 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, secrets, ... }:
 
 {
-
-  # Define acounts account. Don't forget to set a password with ‘passwd’.
+  # Define acounts account.
   users = {
     mutableUsers = false;
     users = {
