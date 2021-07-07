6251347b6b983f84e4e8c1c7bed2640fc9d3807f
Oliver Strik <oliverstrik@gmail.com>
2021-07-07 11:54:17 +0200

    kinda added tyzen studio. It's evil though

--- pkgs/programs/tizen-studio/default.nix
+++ pkgs/programs/tizen-studio/default.nix
@@ -1,4 +1,8 @@
-{ stdenv, fetchurl, autoPatchelfHook, unzip, jdk8 }:
+{ stdenv, fetchurl, autoPatchelfHook, unzip, jdk8,
+  fontconfig, freetype, glib, gsettings-desktop-schemas, gtk2, libX11,
+  libXrender, libXtst, zlib,
+  tizen-data ? "/home/kranex/.tizen-studio-data"
+}:
 let
 
   VERSION = "4.1.1";
@@ -9,7 +13,11 @@ let
 in stdenv.mkDerivation {
   name = "tizen-studio";
 
-  buildInputs = [ jdk8 unzip ];
+  buildInputs = [
+    jdk8 unzip
+    fontconfig freetype glib gsettings-desktop-schemas gtk2 libX11
+    libXrender libXtst zlib
+  ];
 
   src = fetchurl {
     url = TIZEN_URL;
@@ -24,12 +32,26 @@ in stdenv.mkDerivation {
   installPhase = ''
   mkdir -p $out/{bin,tizen-studio}
 
+  # unpack tizen studio
   ${unzip}/bin/unzip tizen-sdk.zip -d $out/tizen-studio
 
+  # we're going to use our own so get rid of this junk.
   rm -rf $out/tizen-studio/jdk
 
+  # Create the stupid sdk.info
   echo TIZEN_SDK_INSTALLED_PATH=$out/tizen-studio > $out/tizen-studio/sdk.info
-  echo TIZEN_SDK_DATA_PATH=\$HOME/tizen-studio-data >> $out/tizen-studio/sdk.info
+  echo TIZEN_SDK_DATA_PATH=${tizen-data} >> $out/tizen-studio/sdk.info
+  echo JDK_PATH=${jdk8} >> $out/tizen-studio/sdk.info
+
+  # replace the stupid sdk.info sdkDataPath with one in the current users home
+  # dir. This is absolutely rediculous btw.
+  # grep -rl 'sdkDataPath=' $out/tizen-studio | \
+  # xargs sed -i '2isdkDataPath=\"\$HOME/tizen-sdk-data\"'
+
+  # update eclipse ini path
+  sed -i "/-vm$/{N;P;s|.*\n||;s|.*|${jdk8}/bin/java|;}" \
+  $out/tizen-studio/ide/eclipse.ini
+  sed -i "s|updateIniFile \".*\"|echo ini already updated by nixos|g" $out/tizen-studio/ide/TizenStudio.sh
   '';
 
 }
