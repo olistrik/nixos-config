c99855c9912bcdd9361736da5753f13cf37c56c0
Kranex <oliverstrik@gmail.com>
2021-07-08 23:33:54 +0200

    feat: nvim overhaul. TODO: extract the langauges and configured plugins.

--- shared/programs/neovim.nix
+++ shared/programs/neovim.nix
@@ -1,76 +1,190 @@
 # Install and configure neovim + plugins.
+{pkgs, lib, ...}:
+let
 
-{pkgs, ...}:
-{
-  environment.systemPackages = with pkgs; [
-    ripgrep
-  ];
-  programs.neovim = {
-    package = pkgs.kranex.neovim-nightly;
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
-    runtime = {
-      "parser/tsx.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-tsx}/parser";
-      "parser/nix.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-nix}/parser";
-      "parser/rb.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-ruby}/parser";
-    };
-    configure = {
-      packages.myPlugins = with pkgs.unstable.vimPlugins; {
-        start = [
-          # Language Support
-          vim-nix
-          vim-pandoc
-          vim-pandoc-syntax
-          vim-ruby
-          vim-rails
-          plantuml-syntax
-          i3config-vim
-          typescript-vim
-          vim-jsx-typescript
+  myNeovim = with pkgs.unstable; neovim-unwrapped.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [ ripgrep ];
+  });
 
-          vim-table-mode
-
-          # QOL
-          vim-gitgutter
-          vim-repeat
-          auto-pairs
-          vim-surround
-
-          # File browsing
-          nerdtree
-          nerdtree-git-plugin
-          fzf-vim
-
-          ###########
-          # 0.5 stuff
-
-          # Telescope
-          popup-nvim
-          plenary-nvim
-          telescope-nvim
-
-          # Treesitter
-          nvim-treesitter
-
-          # IDE
-          ale
-          YouCompleteMe
-
-          # Themes
-          ayu-vim
-        ];
-        opt = [];
-      };
-      customRC = ''
-      " nvimrc
-
-      syntax on
+  configuredPlugins = with pkgs.vimPlugins; {
+    colorscheme = {
+      plugin = ayu-vim;
+      config = ''
       set termguicolors
       let ayucolor="mirage"
       colorscheme ayu
+      '';
+    };
+
+    nerdtree = {
+      plugin = nerdtree;
+      extras = [ nerdtree-git-plugin ];
+      config = ''
+      "" close on file open
+      let g:NERDTreeQuitOnOpen = 1
+
+      "" Open on F2
+      map <F2> :NERDTreeToggle<CR>
+
+      "" close if only thing open.
+      autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+      '';
+    };
+
+    YouCompleteMe = {
+      plugin = YouCompleteMe;
+      config = ''
+      let g:ycm_min_num_of_chars_for_completion = 3
+      let g:ycm_show_diagnostics_ui = 0
+      '';
+    };
+
+    fzf = {
+      plugin = fzf-vim;
+      config = ''
+      let $FZF_DEFAULT_COMMAND = "find -L -not -path '*/\.git/*'"
+      nnoremap <silent> <C-p> :FZF<CR>
+      '';
+    };
+
+    telescope = {
+      plugin = telescope-nvim;
+      extras = [ popup-nvim plenary-nvim ];
+      requires = with pkgs; [ ripgrep ];
+      config = ''
+      nnoremap <silent> <C-p> <cmd>Telescope find_files<cr>
+      nnoremap <silent> <C-f> <cmd>Telescope live_grep<cr>
+      '';
+    };
+
+    treesitter = {
+      plugin = nvim-treesitter;
+      config = ''
+      lua <<EOF
+      require'nvim-treesitter.configs'.setup {
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        }
+      }
+      EOF
+      '';
+      # runtime = ( builtins.listToAttrs (
+      #  lib.mapAttrsToList
+      #      (name: value: {
+      #      name = "parser/${lib.lists.last (lib.strings.splitString "-" name)}.so";
+      #      value = { source = "${value}/parser"; };
+      #      })
+      #      pkgs.tree-sitter.builtGrammars
+      # ));
+    };
+  };
+
+  treeSitterRuntime = name: pkg: {
+    "parser/${name}.so".source = "${pkg}/parser";
+  };
+
+  languages = with pkgs; with vimPlugins; with tree-sitter.builtGrammars; {
+    golang = rec {
+      suffix = "go";
+      runtime = treeSitterRuntime suffix tree-sitter-go;
+    };
+
+    nix = rec {
+      suffix = "nix";
+      extras = [ vim-nix ];
+      # runtime = treeSitterRuntime suffix tree-sitter-nix;
+    };
+
+    c = rec {
+      suffix = "c";
+      runtime = treeSitterRuntime suffix tree-sitter-c;
+    };
+
+    tsx = rec {
+      suffix = "tsx";
+      # extras = [ typescript-vim vim-jsx-typescript ];
+      runtime = treeSitterRuntime suffix tree-sitter-tsx;
+    };
+
+    ruby = rec {
+      suffix = "rb";
+      extras = [ vim-rails ]; # Not sure if i need this?
+      requires = with pkgs.kranex; [ rubocop-sdv ];
+      runtime = treeSitterRuntime suffix tree-sitter-ruby;
+    };
+
+    yaml = rec {
+      suffix = "yaml";
+      runtime = treeSitterRuntime suffix tree-sitter-yaml;
+    };
+  };
+
+  plugins = with configuredPlugins; with languages; [
+    colorscheme
+
+    # file browsing
+    telescope # fzf
+    nerdtree
+
+    # completion
+    # YouCompleteMe
+
+    # linting
+    treesitter
+
+    # languages
+  ] ++ lib.mapAttrsToList (name: value: value) languages;
+
+  runtime = lib.fold (x: y: lib.mergeAttrs x y ) {} (builtins.catAttrs "runtime" plugins);
+
+in {
+  environment.systemPackages = builtins.concatLists (builtins.catAttrs "requires" plugins);
+
+  programs.neovim = {
+    package = pkgs.unstable.neovim-unwrapped;
+    enable = true;
+
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+
+    runtime = builtins.trace runtime runtime;
+
+    configure = {
+      packages.myPlugins = with pkgs.unstable.vimPlugins; {
+        start =
+          builtins.catAttrs "plugin" plugins ++
+          builtins.concatLists (builtins.catAttrs "extras" plugins) ++ [
+            # Language Support
+            vim-pandoc
+            vim-pandoc-syntax
+            plantuml-syntax
+            i3config-vim
+
+            # QOL
+            vim-gitgutter
+            vim-repeat
+            auto-pairs
+            vim-surround
+            #vim-table-mode
+
+            # IDE
+            ale
+          ];
+
+        opt = [];
+      };
+
+      customRC = builtins.concatStringsSep "\n" (builtins.catAttrs "config" plugins) + ''
+      " nvimrc
+
+      syntax on
 
       set number relativenumber
+
       function! GitBranch()
       return system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
       endfunction
@@ -125,57 +239,6 @@
 
       set list
       set listchars=tab:>\ ,trail:•,extends:#,nbsp:.
-
-
-      " NERDTree
-      "" close on file open
-      let g:NERDTreeQuitOnOpen = 1
-
-      "" Open on F2
-      map <F2> :NERDTreeToggle<CR>
-
-      "" close if only thing open.
-      autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
-
-      "" YouCompleteMe
-      let g:ycm_min_num_of_chars_for_completion = 3
-      let g:ycm_show_diagnostics_ui = 0
-
-      "" FZF
-      " let $FZF_DEFAULT_COMMAND = "find -L -not -path '*/\.git/*'"
-      " nnoremap <silent> <C-p> :FZF<CR>
-
-      "" Telescope
-      nnoremap <silent> <C-p> <cmd>Telescope find_files<cr>
-      nnoremap <silent> <C-f> <cmd>Telescope live_grep<cr>
-
-      "" Treesitter
-      lua <<EOF
-      require'nvim-treesitter.configs'.setup {
-        highlight = {
-          enable = true,
-        },
-      }
-      EOF
-
-      "" Vim Table Mode
-      function! s:isAtStartOfLine(mapping)
-      let text_before_cursor = getline('.')[0 : col('.')-1]
-      let mapping_pattern = '\V' . escape(a:mapping, '\')
-      let comment_pattern = '\V' . escape(substitute(&l:commentstring,
-      '%s.*$', ''', '''), '\')
-      return (text_before_cursor =~? '^' . ('\v(' . comment_pattern . '\v)?') . '\s*\v' . mapping_pattern . '\v$')
-      endfunction
-
-      inoreabbrev <expr> <bar><bar>
-              \ <SID>isAtStartOfLine('\|\|') ?
-              \ '<c-o>:TableModeEnable<cr><bar><space><bar><left><left>' : '<bar><bar>'
-      inoreabbrev <expr> __
-              \ <SID>isAtStartOfLine('__') ?
-              \ '<c-o>:silent! TableModeDisable<cr>' : '__'
-
-      let g:table_mode_corner_corner='+'
-      let g:table_mode_header_fillchar='='
       '';
     };
   };
