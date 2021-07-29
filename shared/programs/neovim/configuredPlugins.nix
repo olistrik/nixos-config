f1ab7749b0052d15e50ee37e0afd6761faf4d3da
Kranex <oliverstrik@gmail.com>
2021-07-29 12:07:33 +0200

    add some more vim bindings

--- shared/programs/neovim/configuredPlugins.nix
+++ shared/programs/neovim/configuredPlugins.nix
@@ -141,17 +141,18 @@ with vimPlugins; {
         EOF
 
         " Code navigation shortcuts
-        nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
-        nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
-        nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
-        nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
-        nnoremap <silent> 1gD   <cmd>lua vim.lsp.buf.type_definition()<CR>
-        nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
-        nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
-        nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
-        nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.declaration()<CR>
-        nnoremap <silent> ga    <cmd>lua vim.lsp.buf.code_action()<CR>
-
+        nnoremap <silent> gd         <cmd>lua vim.lsp.buf.definition()<CR>
+        nnoremap <silent> K          <cmd>lua vim.lsp.buf.hover()<CR>
+        nnoremap <silent> gD         <cmd>lua vim.lsp.buf.implementation()<CR>
+        nnoremap <silent> <c-k>      <cmd>lua vim.lsp.buf.signature_help()<CR>
+        nnoremap <silent> 0gD        <cmd>lua vim.lsp.buf.type_definition()<CR>
+        nnoremap <silent> gr         <cmd>lua vim.lsp.buf.references()<CR>
+        nnoremap <silent> g-1        <cmd>lua vim.lsp.buf.document_symbol()<CR>
+        nnoremap <silent> gW         <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
+        nnoremap <silent> <c-]>      <cmd>lua vim.lsp.buf.declaration()<CR>
+        nnoremap <silent> <Leader>rn <cmd>lua vim.lsp.buf.rename()<CR>
+        nnoremap <silent> ga         <cmd>lua vim.lsp.buf.code_action()<CR>
+        nnoremap <silent> <Leader>fm <cmd>lua vim.lsp.buf.formatting()<CR>
 
         " Set updatetime for CursorHold
         " 300ms of no cursor movement to trigger CursorHold
