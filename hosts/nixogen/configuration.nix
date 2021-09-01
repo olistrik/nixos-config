fd857bab4507b028ae3862bcc5782981ef442546
Oliver Strik <oliver@klippa.com>
2021-09-01 11:24:55 +0200

    fix: typo

--- hosts/nixogen/configuration.nix
+++ hosts/nixogen/configuration.nix
@@ -9,8 +9,8 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
 
-      ../shared/personal.nix
-      ../shared/work.nix
+      ../../shared/personal.nix
+      ../../shared/work.nix
     ];
 
   nix = {
