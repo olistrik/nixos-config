88ca28a713d87aebe59f4fe32713605f2494528f
Kranex <oliverstrik@gmail.com>
2021-09-15 10:06:14 +0200

    switch to sddm (3)

--- shared/wm/sway/default.nix
+++ shared/wm/sway/default.nix
@@ -8,10 +8,10 @@ in {
 
   services.xserver = {
     enable = true;
+    libinput.enable = true;
     displayManager = {
       defaultSession = "sway";
       sddm.enable = true;
-      libinput.enable = true;
     };
   };
 
