1bea634fd205f2a36a9efec3d43c5d752505a6a2
Kranex <oliverstrik@gmail.com>
2021-12-03 08:37:59 +0100

    disable openrgb

--- hosts/nixium/rgb.nix
+++ hosts/nixium/rgb.nix
@@ -7,15 +7,15 @@
 
   hardware.i2c.enable = true;
 
-  systemd.services.openrgbBoot = {
-    wantedBy = [ "multi-user.target" "suspend.target" ];
-    after = [ "suspend.target" ];
-    script = ''
-      echo setting strips to length 16
-      ${pkgs.openrgb}/bin/openrgb -d 4 -z 1 -s 16 -z 2 -s 16
+  # systemd.services.openrgbBoot = {
+  #   wantedBy = [ "multi-user.target" "suspend.target" ];
+  #   after = [ "suspend.target" ];
+  #   script = ''
+  #     echo setting strips to length 16
+  #     ${pkgs.openrgb}/bin/openrgb -d 4 -z 1 -s 16 -z 2 -s 16
 
-      echo setting RGB to off white 8899FF
-      ${pkgs.openrgb}/bin/openrgb -c 8899FF
-    '';
-  };
+  #     echo setting RGB to off white 8899FF
+  #     ${pkgs.openrgb}/bin/openrgb -c 8899FF
+  #   '';
+  # };
 }
