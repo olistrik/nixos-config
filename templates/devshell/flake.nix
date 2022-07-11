3b9beeb51c09c4019e353d51e99efbade87318d5
Kranex <oliverstrik@gmail.com>
2022-07-11 08:23:07 +0200

    add devshell template

---
+++ templates/devshell/flake.nix
@@ -0,0 +1,20 @@
+# https://github.com/numtide/devshell
+{
+  description = "nix is love, nix is life";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, devshell, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell = let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlay ];
+        };
+      in pkgs.devshell.mkShell {
+        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+      };
+    });
+}
