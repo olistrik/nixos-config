3ce6d3c002e173098fc38e7e9c918f0c5788c4d1
Kranex <oliverstrik@gmail.com>
2022-12-23 12:00:56 +0100

    add cachix and devenv

---
+++ cachix/devenv.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://devenv.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
+}
