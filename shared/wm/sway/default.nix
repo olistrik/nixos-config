8831670ea3bd5088b3f79fe41b82044d6d49d0b0
Kranex <oliverstrik@gmail.com>
2023-07-31 09:56:16 +0200

    update lock screen

--- shared/wm/sway/default.nix
+++ shared/wm/sway/default.nix
@@ -31,18 +31,18 @@ in {
       playerctl
 
       (pkgs.writeScriptBin "lock" ''
-        ${swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator-idle-visible \
-        --indicator-radius 100 \
-        --indicator-thickness 7 \
-        --ring-color 455a64 \
-        --key-hl-color be5046 \
-        --text-color ffc107 \
-        --line-color 00000000 \
-        --inside-color 00000088 \
-        --separator-color 00000000 \
-        --fade-in 0.1 \
-        --effect-pixelate 16 \
-        "$@"
+                ${swaylock-effects}/bin/swaylock -f --image "$HOME/screensaver" 
+        		--clock \
+                --indicator-radius 100 \
+                --indicator-thickness 7 \
+                --ring-color 455a64 \
+                --key-hl-color be5046 \
+                --text-color ffc107 \
+                --line-color 00000000 \
+                --inside-color 00000088 \
+                --separator-color 00000000 \
+                --fade-in 0.1
+                "$@"
       '')
       (pkgs.writeScriptBin "ssh-add-all" ''
         exec ls -d ~/.ssh/* | grep -vE 'known_hosts|.pub$'| xargs ssh-add
