d75e719b38110b8abe90996227e05ab4ffefc501
Kranex <oliverstrik@gmail.com>
2023-09-16 20:54:15 +0200

    foo

--- shell.nix
+++ shell.nix
@@ -1,24 +1,18 @@
 { pkgs ? import <nixpkgs> { } }:
-
 with pkgs;
 mkShell {
   name = "My-project build environment";
   buildInputs = lib.mapAttrsToList (name: value: (writeScriptBin name value)) {
-    test = ''
-      	  echo "HELLO"
-            git add -A
-            nix-rebuild test --flake ".#"
+    build = ''
+      git add -A
+      nix-rebuild test --flake ".#"
     '';
     switch = ''
       git add -A
       [[ -z $(git status -s) ]] || git commit
-      sudo nixos-rebuild switch --flake ".#"
+      nixos-rebuild switch --flake ".#"
       [[ -z $(git status -s) ]] || git commit -am "chore: update flake.lock"
       git push
     '';
   };
-  shellHook = ''
-    echo "Welcome in $name"
-    export FOO="BAR"
-  '';
 }
