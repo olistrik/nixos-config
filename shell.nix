11404f31826061377bdb4a4241a710fa6f214d52
olistrik <oliverstrik@gmail.com>
2024-01-03 17:11:28 +0100

    stop doing weird permission hacks

--- shell.nix
+++ shell.nix
@@ -14,12 +14,6 @@ mkShell {
       [[ -z $(git status -s) ]] || git commit -am "chore: update flake.lock"
       git push
     '';
-    fix-perms = ''
-      chown -R root:wheel ../nixos
-      find ../nixos -type d -exec chmod 775 {} +
-      find ../nixos -type f -exec chmod 664 {} +
-      find ../nixos/scripts -type f -exec chmod 755 {} +
-    '';
     update = "nix flake update";
     build-iso = "sudo nix build .#liveUsb";
   };
