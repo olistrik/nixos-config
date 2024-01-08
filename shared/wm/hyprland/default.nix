e75244b680e052654b617bdca3123b0257d46c03
olistrik <oliverstrik@gmail.com>
2024-01-08 14:07:21 +0100

    use 'latest' hyprland

--- shared/wm/hyprland/default.nix
+++ shared/wm/hyprland/default.nix
@@ -2,6 +2,7 @@
 	let themer = config.system.themer;
 in {
 	programs.hyprland.enable = true;
+	programs.hyprland.package = pkgs.unstable.hyprland;
 
 	environment.systemPackages = with pkgs; [
 		hyprpaper
