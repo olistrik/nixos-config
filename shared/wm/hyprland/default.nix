511a95c718581ba0cf83fdfb39076124daaec876
olistrik <oliverstrik@gmail.com>
2024-01-11 09:41:22 +0100

    switch to pipewire

--- shared/wm/hyprland/default.nix
+++ shared/wm/hyprland/default.nix
@@ -1,10 +1,18 @@
 { config, lib, pkgs, ... }: 
 	let themer = config.system.themer;
 in {
-	programs.hyprland.enable = true;
-
 	environment.systemPackages = with pkgs; [
 		hyprpaper
 		swaylock
 	];
+
+	programs.hyprland.enable = true;
+
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+	};
 }
