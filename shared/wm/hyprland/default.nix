c6df69fb1bef05b205b6d1157d3f0021187870c6
olistrik <oliverstrik@gmail.com>
2024-01-11 09:47:00 +0100

    fix slack and some screenshares

--- shared/wm/hyprland/default.nix
+++ shared/wm/hyprland/default.nix
@@ -6,6 +6,8 @@ in {
 		swaylock
 	];
 
+	environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
 	programs.hyprland.enable = true;
 
 	security.rtkit.enable = true;
@@ -14,5 +16,14 @@ in {
 		alsa.enable = true;
 		alsa.support32Bit = true;
 		pulse.enable = true;
+	};
+
+	xdg.portal = {
+		enable = true;
+		extraPortals = with pkgs; [
+			xdg-desktop-portal-wlr
+			xdg-desktop-portal-gtk
+		];
+		gtkUsePortal = true;
 	};
 }
