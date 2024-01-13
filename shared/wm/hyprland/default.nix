33418205b24f734c2b449f290020ff9de54015ea
olistrik <oliverstrik@gmail.com>
2024-01-13 23:17:19 +0100

    fix swaylock

--- shared/wm/hyprland/default.nix
+++ shared/wm/hyprland/default.nix
@@ -4,12 +4,12 @@
 	];
 
   programs.hyprland.enable = true;
-
+  security.pam.services.swaylock = {};
 	# FROM SWAY:
 	# extraSessionCommands = ''
-	# 	export MOZ_ENABLE_WAYLAND=1
-	# 	export _JAVA_AWT_WM_NONREPARENTING=1
-	# 	systemctl --user import-environment
+	#		export MOZ_ENABLE_WAYLAND=1
+	#		export _JAVA_AWT_WM_NONREPARENTING=1
+	#		systemctl --user import-environment
 	# '';
 
   environment.systemPackages = with pkgs; [
