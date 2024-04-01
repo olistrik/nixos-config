37b3f341ed7d75f381b4ce8843450a9579abf4d0
olistrik <oliverstrik@gmail.com>
2024-04-01 21:18:31 +0200

    ssh auth pam stuff is weird

--- modules/nixos/collections/server/default.nix
+++ modules/nixos/collections/server/default.nix
@@ -15,6 +15,18 @@ in
         PermitRootLogin = "no";
         PasswordAuthentication = false;
       };
+      authorizedKeysFiles = lib.mkForce [
+        "/etc/ssh/authorized_keys.d/%u" 
+      ];   
     };
+
+    olistrik.user.authorizedKeys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIL0q7NgpGYIG6vQqzJTD64jUASuDWxw7DNKsrw+j3G/ oli@nixogen"
+    ];
+
+    # Allow sudo for nixos-rebuild --use-remote-sudo, without passwordless sudo.
+    # security.pam.sshAgentAuth.authorizedKeysFiles = lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ];   
+    security.pam.enableSSHAgentAuth = true;
+    security.pam.services.sudo.sshAgentAuth = true;
   };
 }
--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -3,12 +3,16 @@ with lib;
 with lib.olistrik;
 let
   cfg = config.olistrik.user;
+  authorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIL0q7NgpGYIG6vQqzJTD64jUASuDWxw7DNKsrw+j3G/ oli@nixogen" ];
 in
 {
   options.olistrik.user = with types; {
+    enable = mkOpt bool false "Whether to enable my user settings";
     name = mkOpt str "oli" "The name of the standard account";
     initialPassword = mkOpt str "password" "The initial password for the standard account";
     extraGroups = mkOpt (listOf str) [ ] "Any extra groups the default account should have";
+    authorizedKeys = mkOpt (listOf str) [] "";
   };
 
   config.users.users.${cfg.name} = {
@@ -31,5 +35,7 @@ in
       "lxd"
       "openvpn"
     ] ++ cfg.extraGroups;
+    
+    openssh.authorizedKeys.keys = authorizedKeys;
   };
 }
