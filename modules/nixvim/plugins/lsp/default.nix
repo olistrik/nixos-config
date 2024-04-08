a57cf34a42f583432b142148c5583521911e8a37
olistrik <oliverstrik@gmail.com>
2024-04-08 14:27:20 +0200

    enable valheim server

--- modules/nixvim/plugins/lsp/default.nix
+++ modules/nixvim/plugins/lsp/default.nix
@@ -42,26 +42,28 @@ mkPlugin "lsp" {
       };
     };
 
-    nvim-cmp = {
+    cmp = {
       enable = true;
 
-      snippet.expand = "luasnip";
+      settings = {
+        snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
 
-      mapping = {
-        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
-        "<C-Space>" = "cmp.mapping.complete()";
-        "<C-e>" = "cmp.mapping.close()";
-        "<CR>" = "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert })";
-        "<Tab>" = "cmp.mapping.select_next_item()";
-        "<S-Tab>" = "cmp.mapping.select_prev_item()";
+        mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<CR>" = "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert })";
+          "<Tab>" = "cmp.mapping.select_next_item()";
+          "<S-Tab>" = "cmp.mapping.select_prev_item()";
+        };
+
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "buffer"; }
+          { name = "path"; }
+        ];
       };
-
-      sources = [
-        { name = "nvim_lsp"; }
-        { name = "buffer"; }
-        { name = "path"; }
-      ];
     };
 
     lsp-format.enable = true;
