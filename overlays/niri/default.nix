c554a4d56d86cc0ad0a1eaf27504d68fb28e24ba
olistrik <oliverstrik@gmail.com>
2024-11-01 13:47:43 +0100

    upstream now builds xwayland satellite with systemd enabled

--- overlays/niri/default.nix
+++ overlays/niri/default.nix
@@ -1,7 +1,5 @@
 { inputs, ... }: final: prev: {
   # nixpkgs is behind, unstable breaks mesa, niri-flake.nixosModules does a little too much.
   niri = prev.niri-stable;
-  xwayland-satellite = prev.xwayland-satellite-unstable.overrideAttrs (finalAttrs: prevAttrs: {
-    cargoBuildFlags = [ "--features=systemd" ];
-  });
+  xwayland-satellite = prev.xwayland-satellite-unstable;
 }
