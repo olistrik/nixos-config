3b6a5e4895b0940e05682b7927730fd195f9217d
olistrik <oliverstrik@gmail.com>
2025-03-18 13:49:56 +0100

    switch valheim to new fork

--- modules/nixos/services/valheim-server/default.nix
+++ modules/nixos/services/valheim-server/default.nix
@@ -16,15 +16,28 @@ in
       "The name of the world in the server list.";
     password = mkOpt str "12345"
       "The password for the server.";
+    # passwordEnvFile = ...;
     openFirewall = mkOpt bool true
       "Whether to open the firewall or not.";
+    noGraphics = mkOpt bool true
+      "If enabled, disables initalization of the graphics device.";
+    public = mkOpt bool false
+      "If enabled, shows the server on the steam and community lists.";
     extraOptions = mkOpt (attrsOf anything) { }
       "Extra options for upstream valheim server module.";
   };
 
   config = lib.mkIf cfg.enable {
     services.valheim = {
-      inherit (cfg) enable serverName worldName password openFirewall;
+      inherit (cfg)
+        enable
+        serverName
+        worldName
+        password
+        openFirewall
+        noGraphics
+        public
+        ;
     } // cfg.extraOptions;
   };
 }
