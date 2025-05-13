6554b737918c4b79eb38d977ce5f0d443f0be001
olistrik <oliverstrik@gmail.com>
2025-05-13 09:14:54 +0200

    boostrap loki; my new nixos WSL

---
+++ systems/x86_64-linux/loki/default.nix
@@ -0,0 +1,50 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ lib, pkgs, ... }:
+with lib; with olistrik;
+{
+  nixpkgs.config.allowUnfree = true;
+
+  wsl = {
+    enable = true;
+    defaultUser = "oli";
+    useWindowsDriver = true;
+  };
+
+  olistrik = {
+    collections = {
+      server = enabled;
+    };
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
+
+  environment.sessionVariables = {
+    CUDA_PATH = "${pkgs.cudatoolkit}";
+    EXTRA_LDFLAGS = "-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib";
+    EXTRA_CCFLAGS = "-I/usr/include";
+    LD_LIBRARY_PATH = [
+      "/usr/lib/wsl/lib"
+      "${pkgs.linuxPackages.nvidia_x11}/lib"
+      "${pkgs.ncurses5}/lib"
+      "/run/opengl-driver/lib"
+    ];
+    MESA_D3D12_DEFAULT_ADAPTER_NAME = "Nvidia";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
+
+
