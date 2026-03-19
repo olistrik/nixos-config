ede1d442ae465776148faf29b7029dcf6d0fb1bf
Oli Strik <strik@olii.nl>
2026-03-19 16:57:18 +0100

    remove some comments

--- lib.nix
+++ lib.nix
@@ -6,30 +6,20 @@ let
   lib = pkgs.lib;
   inherit ((import ./lib/loader/list-files.nix { inherit lib; }).loader) listFiles;
 
-  # 1. Get the list of files (excluding underscore-prefixed)
   libFiles = listFiles ./lib;
 
-  # 2. Define the library loader
-  # We use lib.fix here so that 'self' is available inside every function
   finalLib = lib.fix (
     final:
     let
-      # Context to be passed to every library file
       ctx = {
         inherit lib pkgs;
         self = {
           inherit sources;
-          # if lib needs anything else; it will go in here.
           lib = final;
         };
       };
-
-      # Import and apply context to every file
-      # This results in a list of nested attrsets: [{ foo.bar = ...; }]
       appliedFiles = map (f: import f ctx) libFiles;
 
-      # Deep merge the results into one tree
-      # lib.recursiveUpdate works perfectly for standard attrsets
     in
     builtins.foldl' lib.recursiveUpdate { } appliedFiles
   );
