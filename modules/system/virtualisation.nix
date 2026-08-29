8811b6547a9ebbfb4b1169f28878a112eecfeacf
olistrik <strik@olii.nl>
2026-08-29 12:00:01 +0200

    fix: use rootless docker

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -1,11 +1,15 @@
 {
   nixos.system.virtualisation =
-    { lib, ... }:
+    { ... }:
     {
+      # Rootless Docker runs as the invoking user, avoiding the rootful daemon
+      # and its root-equivalent `docker` group.
       virtualisation.docker = {
-        enable = true;
+        enable = false;
+        rootless = {
+          enable = true;
+          setSocketVariable = true;
+        };
       };
-
-      users.users.oli.extraGroups = [ "docker" ];
     };
 }
