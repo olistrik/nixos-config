1f7ceea4032961bb71ccbe49ed729b0e66dbddef
olistrik <strik@olii.nl>
2026-09-14 11:32:42 +0200

    feat: add measured boot

--- modules/hosts/kvasir/bootloader.nix
+++ modules/hosts/kvasir/bootloader.nix
@@ -22,9 +22,26 @@
       boot.loader.efi.canTouchEfiVariables = true;
       boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
+      boot.initrd.systemd.enable = true;
+
       boot.lanzaboote = {
         enable = true;
         pkiBundle = "/var/lib/sbctl";
+
+        # systemd-pcrlock enforces this hard cap once measured boot is on.
+        configurationLimit = 8;
+
+        # PCRs 1-3 are flaky across firmware/hardware; 0 (firmware), 4
+        # (boot loader/kernel image) and 7 (secure boot state) are the
+        # set lanzaboote's own docs recommend and test against.
+        measuredBoot = {
+          enable = true;
+          pcrs = [
+            0
+            4
+            7
+          ];
+        };
       };
     };
 }
